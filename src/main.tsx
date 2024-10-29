e7fae26c65bcb0e5eb17a404a921cfcb9991597a
diegolentz <diegoolentz@gmail.com>
2024-10-29 12:47:59 -0300

    card de autor

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Dashboard } from './componentes/dashboard/Dashboard.tsx'
+import { Author } from './componentes/Author/Author.tsx'
+Author
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Dashboard></Dashboard>
+    {/* <Dashboard></Dashboard> */}
+    <Author></Author>
+
+
   </StrictMode>,
 )
