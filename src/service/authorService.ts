a38f8b0b0fa03c88e58ba82124fcfbc491734827
diegolentz <diegoolentz@gmail.com>
2024-10-30 18:55:30 -0300

    me traigo los autores

--- src/service/authorService.ts
+++ src/service/authorService.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
 import { REST_SERVER_URL } from "../constants";
-// import { AuthorJSON } from "../domain/AuthorJSON";
+import { AuthorJSON,AuthorJson } from "../domain/AuthorJSON";
 
 class AuthorService {
-    // async getAuthorData(): Promise<AuthorJSON> {
-    //     const data = await axios.get(REST_SERVER_URL + "/author");
-    //     return AuthorJSON.fromJson(data.data);
-    // }
-
-    
+    async getAuthorData(): Promise<AuthorJSON[]> {
+        const data = await axios.get(REST_SERVER_URL + "/reactAutor");
+        // Asegúrate de mapear cada elemento del array a una instancia de AuthorJSON
+        return data.data.map((item: any) => AuthorJson.fromJson(item));
+    }
 }
 
 export const authorService = new AuthorService();
