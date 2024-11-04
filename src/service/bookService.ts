2c20f025ba02b16368d7c7d058391450b370e9c0
borrellidelfina <borrellidelfina@gmail.com>
2024-11-04 18:51:22 -0300

    Edicion Book

--- src/service/bookService.ts
+++ src/service/bookService.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { REST_SERVER_URL } from "../constants";
-import { BookJson, BookJSON } from "../domain/BookJSON";
+import { Book, BookJSON } from "../domain/BookJSON";
 
 class BookService {
-    async getBooksShortData(): Promise<BookJSON[]> {
+    async getBooksShortData(): Promise<Book[]> {
         const books = await axios.get(REST_SERVER_URL + "/getBooksReact");
-        return books.data.map((item: BookJSON) => BookJson.fromJson(item));
+        console.log(books)
+        const books2 = books.data.map((item: BookJSON) => Book.prototype.fromJson(item))
+        console.log(books2)
+        return books.data.map((item: BookJSON) => Book.prototype.fromJson(item));
     }
 }
 
