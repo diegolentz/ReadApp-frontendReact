34bb96abfcdbaab866975f361c26d432456b1a6a
ValentinPg <105302328+ValentinPg@users.noreply.github.com>
2024-11-07 21:16:06 -0300

    Utilizo MUI para las cards

--- src/componentes/dashboard/DashboardCard/DashboardCard.tsx
+++ src/componentes/dashboard/DashboardCard/DashboardCard.tsx
@@ -1,10 +1,18 @@
 import './DashoardCard.css'
+import { Card } from '@mui/material';
 
-export const DashboardCard = ({svg, title, data} : {svg:string, title:string, data:number}) =>
-    <article className="dashboard-item">
+export const DashboardCard = ({ svg, title, data }: { svg: string, title: string, data: number }) =>
+
+    <Card className='dashboard-item'><img src={svg} alt="ICON" />
+        <div className="dashboard-description">
+            <h1>{data}</h1>
+            <h4>{title}</h4>
+        </div>
+    </Card>
+{/* <article className="dashboard-item">
                 <img src={svg} alt="ICON" />
                 <div className="dashboard-description">
                 <h2>{data}</h2>
                 <h4>{title}</h4>
                 </div>
-        </article>
+        </article> */}
