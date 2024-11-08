063f62da5938beac2e1bb66b30e8267500b5eebe
diegolentz <diegoolentz@gmail.com>
2024-11-07 21:20:24 -0300

    card ok

--- src/componentes/FolderAuthor/Author/Author.tsx
+++ src/componentes/FolderAuthor/Author/Author.tsx
@@ -3,6 +3,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { AuthorJSON } from "../../../domain/AuthorJSON";
 import { Height } from "@mui/icons-material";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 export const Author = ({
     renderAuthor,
@@ -29,95 +30,94 @@ export const Author = ({
     };
 
     return (
-        <Box 
-            display="flex" 
-            flexDirection="column" 
-            alignItems="center" 
-            gap={1.5} 
-            width="100%" 
-            height= "100%"
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            gap={1.5}
+            width="100%"
+            height="100%"
+            padding={2} 
         >
             {renderAuthor.map((author) => (
 
-                <Card 
-                key={author.id} variant="outlined" sx={{ 
-                        display: "flex", 
+                <Card
+                    key={author.id} variant="outlined" sx={{
+                        display: "flex",
                         alignItems: "center",
-                        padding: 1, 
-                        width: 250, 
+                        padding: 1,
+                        width: 250,
                         height: 100,
                         borderRadius: 2,
-                        borderColor: '#d50000',
-                        backgroundColor: "#e57373",
+                        borderColor: '#212121',
+                        backgroundColor: "#ff8a80",
                     }}>
 
-                    <CardContent onClick={() => showAuthor(author.id)} sx={{ 
-                            flexGrow: 1, 
-                            cursor: "pointer",
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "start",
-                            gap: 2,
+                    <CardContent onClick={() => showAuthor(author.id)} sx={{
+                        flexGrow: 1,
+                        cursor: "pointer",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "start",
+                        gap: 2,
 
-                        }}>
-                        <Typography 
-                            variant="h5" 
+                    }}>
+                        <Typography
+                            variant="h5"
                             color="text.primary"
+                            sx={{ fontWeight: 'bold' }}
                         >
                             {author.name} {author.lastName}
                         </Typography>
 
-                        <Typography 
+                        <Typography
                             variant="h6"
-                            color="text.secondary"
+                            color="text.primary"
+                            sx={{ fontStyle: 'oblique' }}
                         >
                             {author.nationality}
                         </Typography>
 
                     </CardContent>
-                    <Box 
-                        display="flex" 
-                        flexDirection="column" 
-                        alignItems="center"  
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        alignItems="center"
                         justifyContent="space-between"
-                        width="10%"
+                        width="20%"
                         height="100%"
                         gap={0.5}
-
+                    >
+                        <IconButton
+                            onClick={() => showAuthor(author.id)}
+                            color="default"
+                            sx={{ height: "33%", padding: 0 }}
                         >
-                        
-                        <IconButton 
-                            onClick={() => editAuthor(author.id)} 
+                            <MoreVertIcon sx={{ width: "100%", height: "100%" }} />
+                        </IconButton>
+
+                        <IconButton
+                            onClick={() => editAuthor(author.id)}
                             color="success"
-                             
-                            sx={{height: "33%",
+                            sx={{
+                                height: "33%",
                                 padding: 0,
                             }}
-                            >
-                            <EditIcon sx={
-                                { height: "auto" }
-                            }/>
+                        >
+                            <EditIcon sx={{ width: "100%", height: "100%" }} />
                         </IconButton>
-
-
+                        
                         {!author.creator && (
-                            <IconButton 
-                            sx={{ height: "33%" }}
-                                onClick={() => deleteAuthor(author.id)} 
-                                color="error" 
-                                size="small">
-
-                                <DeleteIcon fontSize="small" />
+                            <IconButton
+                                sx={{ height: "33%", padding: 0 }}
+                                onClick={() => deleteAuthor(author.id)}
+                                color="error"
+                            >
+                                <DeleteIcon sx={{ width: "100%", height: "100%" }} />
                             </IconButton>
                         )}
-                        <IconButton 
-                            onClick={() => showAuthor(author.id)} 
-                            sx={{ height: "33%" }}
-                            color="primary" 
-                            >
-
-                            <EditIcon fontSize="small" />
-                        </IconButton>
+                        
+                     
                     </Box>
                 </Card>
             ))}
